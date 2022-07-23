66ac52846519e72deb1fe6f153e630b2f62cd7ad
Snider <snider@lt.hn>
2022-07-23 11:51:26 +0100

    OK, Finally; decided on the folder structure for an app, CI file is more reusable.

--- app-miner-xmrig/src/app/modules/pool/pool.module.ts
+++ app-miner-xmrig/src/app/modules/pool/pool.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PoolComponent } from './pool.component';
-import {ToolbarModule} from '@swimlane/ngx-ui';
-import {MomentModule} from 'ngx-moment';
-import {FlexModule} from '@angular/flex-layout';
-import {NgChartsModule} from 'ng2-charts';
-import {MatListModule} from '@angular/material/list';
-import {PipesModule} from 'src/app/pipes/pipes.module';
-
-
+import { ToolbarModule } from '@swimlane/ngx-ui';
+import { MomentModule } from 'ngx-moment';
+import { FlexModule } from '@angular/flex-layout';
+import { NgChartsModule } from 'ng2-charts';
+import { MatListModule } from '@angular/material/list';
+import { PipesModule } from 'src/app/pipes/pipes.module';
 
 @NgModule({
   declarations: [
